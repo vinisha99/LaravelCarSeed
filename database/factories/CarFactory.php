ed0ac651ada87834237484c773fa404e060f2c9e
vinisha <vinisha99@gmail.com>
2018-11-01 19:56:06 -0400

    Task: Created new factory 'CarFactory' for model 'Car'

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        'make' => $faker->randomElement($modelArray = array('Ford', 'Honda', 'Toyota')),
+        'model'=> $faker->randomElement($makeArray = array('Convertable', 'Coupe', 'Suv', 'Crossover', 'Hatchback', 'Wagon')),
+        'year' => $faker->year,
+    ];
+});

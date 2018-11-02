5ee13266821e194020ba12f7feb8e8ca7637307e
vinisha <vinisha99@gmail.com>
2018-11-02 02:06:18 -0400

    Task: Feature Test to assert status of About Page

---
+++ tests/Feature/AboutReturnTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AboutReturnTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAboutTest()
+    {
+        $response = $this->get('/about');
+
+        $response->assertStatus(200);
+    }
+}

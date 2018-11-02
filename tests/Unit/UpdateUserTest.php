0009a5bebe67d72018128c529aa288d62c9c52f4
vinisha <vinisha99@gmail.com>
2018-11-02 18:54:42 -0400

    Task: Unit test to update user name in the Users Table

---
+++ tests/Unit/UpdateUserTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class UpdateUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $findUser = User::inRandomOrder()->first();
+        $this->asserttrue($findUser->update(['name' => 'Steve Smith']));
+    }
+}
